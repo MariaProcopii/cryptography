a28816def92dab18d6359b8da629afcccc076bd1
MariaProcopii <mari.procopii@gmail.com>
2023-09-11 13:21:59 +0300

    did the second ex

--- src/main/java/org/example/CaesarC.java
+++ src/main/java/org/example/CaesarC.java
@@ -1,45 +1,104 @@
 package org.example;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class CaesarC {
-    static HashMap<String, Integer> alphabet = new HashMap<>();
+    static HashMap<String, Integer> findInt = new HashMap<>();
+    static HashMap<Integer, String> findChar = new HashMap<>();
 
-    public static void makeAlpha(){
-        for(int i = 0; i < 26; i ++){
-            char c = (char) (i + 'A');
-            alphabet.put(String.valueOf((c)), i);
-        }
-    }
+    static HashMap<String, Integer> findIntShift = new HashMap<>();
+    static HashMap<Integer, String> findCharShift = new HashMap<>();
+
 
     public static String encrypt(String message, int key) {
+        makeAlpha();
         StringBuilder encryptedMessage = new StringBuilder();
 
         for (char c : message.toCharArray()) {
-                char encrChar = (char) ((c - 'A' + key) % 26 + 'A');
-                encryptedMessage.append(encrChar);
+            int code = findInt.get(String.valueOf(c));
+            int encrInt = ( code + key ) % 26;
+            encryptedMessage.append(findChar.get(encrInt));
             }
 
         System.out.println(encryptedMessage);
         return encryptedMessage.toString();
     }
 
+    public static String encrypt(String message, int key, String key2){
+
+        shiftAlpha(key2);
+        StringBuilder encryptedMessage = new StringBuilder();
+
+        for (char c : message.toCharArray()) {
+            int code = findInt.get(String.valueOf(c));
+            int encrInt = ( code + key ) % 26;
+            encryptedMessage.append(findCharShift.get(encrInt));
+        }
+
+        System.out.println(encryptedMessage);
+        return encryptedMessage.toString();
+
+    }
+
     public static String decrypt(String message, int key) {
+        makeAlpha();
         StringBuilder decryptedMessage = new StringBuilder();
 
         for (char c : message.toCharArray()) {
-            char decrChar = (char) ((c - 'A' - key + 26) % 26 + 'A');
-            decryptedMessage.append(decrChar);
+            int code = findInt.get(String.valueOf(c));
+            int decrInt = (code - key + 26) % 26 ;
+            decryptedMessage.append(findChar.get(decrInt));
         }
 
         System.out.println(decryptedMessage);
         return decryptedMessage.toString();
-
     }
 
-    public static void main(String[] args) {
+    public static String decrypt(String message, int key, String key2) {
+        shiftAlpha(key2);
+        StringBuilder decryptedMessage = new StringBuilder();
+
+        for (char c : message.toCharArray()) {
+            int code = findIntShift.get(String.valueOf(c));
+            int decrInt = (code - key + 26) % 26 ;
+            decryptedMessage.append(findChar.get(decrInt));
+        }
+
+        System.out.println(decryptedMessage);
+        return decryptedMessage.toString();
+    }
+
+    public static void makeAlpha(){
+        for(int i = 0; i < 26; i ++){
+            char c = (char) (i + 'A');
+            findInt.put(String.valueOf((c)), i);
+            findChar.put(i, String.valueOf((c)));
+        }
+    }
+
+    public static void shiftAlpha(String key2){
         makeAlpha();
-    }
+        key2 = key2.toUpperCase();
+        LinkedHashSet<String> uniqueChar = new LinkedHashSet<>(); //chars from key2
+        for(char c : key2.toCharArray()){
+            uniqueChar.add(String.valueOf(c));
+        }
 
+        ArrayList<String> uniqueCharArray = new ArrayList<>(uniqueChar);
+
+        for(int i = 0; i < uniqueCharArray.size(); i++){
+            findCharShift.put(i, uniqueCharArray.get(i));
+            findIntShift.put(uniqueCharArray.get(i), i);
+        }
+
+        int count = uniqueCharArray.size();
+        for(int i = 0; i < 26; i++){
+            if(!uniqueCharArray.contains(findChar.get(i))){
+                findCharShift.put(count, findChar.get(i));
+                findIntShift.put(findChar.get(i), count);
+                count++;
+            }
+        }
+    }
 }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,19 +9,21 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String choice;
 
-            System.out.println("(E): Encryption ");
-            System.out.println("(D): Decryption");
+            System.out.println("(e): Encryption with 1 key");
+            System.out.println("(d): Decryption with 1 key");
+            System.out.println("(E): Encryption with 2 keys");
+            System.out.println("(D): Decryption with 2 keys");
 
             System.out.print("Enter your choice: ");
             choice = scanner.nextLine();
 
 
             while (true){
-                if(choice.equals("E") || choice.equals("D")){
+                if(choice.equals("E") || choice.equals("D") || choice.equals("e") || choice.equals("d")){
                     break;
                 }
                 else{
-                    System.out.println("Invalid choice.");
+                    System.out.println("[Invalid choice. You can pick just (E) (d) (E) (D)]");
                     System.out.print("Enter your choice: ");
                     choice = scanner.nextLine();
                 }
@@ -41,6 +43,10 @@ public class Main {
         return true;
     }
 
+    private static boolean isValidKey2(String key2) {
+        return key2.length() >= 7;
+    }
+
     private static boolean isValidKey(int key){
         if(key > 25 || key < 1){
             return false;
@@ -49,32 +55,48 @@ public class Main {
     }
 
     private static void scanningProcess(Scanner scanner, String choice){
-        System.out.println("Are allowed only characters (A-Z, a-z)");
+        System.out.println("[Are allowed only characters (A-Z, a-z).]");
         System.out.print("Enter your message: ");
         String message = scanner.nextLine();
 
         message = message.replace(" ", "").toUpperCase();
 
         while (!isValidMessage(message)){
-            System.out.println("Invalid characters. Are allowed only characters (A-Z, a-z)");
+            System.out.println("[Invalid characters. Are allowed only characters (A-Z, a-z).]");
             System.out.print("Enter your message: ");
             message = scanner.nextLine().replace(" ", "").toUpperCase();
         }
 
-        System.out.println("Valid key should be between 1 and 25");
+        String key2 = null;
+        if(choice.equals("E") || choice.equals("D")){
+            System.out.println("[Are allowed only characters (A-Z, a-z), length >= 7.]");
+            System.out.print("Enter your key2: ");
+            key2 = scanner.nextLine();
+
+            key2 = key2.replace(" ", "").toUpperCase();
+
+            while (!(isValidMessage(key2) && isValidKey2(key2))){
+                System.out.println("[Invalid choice. Are allowed only characters (A-Z, a-z), length >= 7.]");
+                System.out.print("Enter your key2: ");
+                key2 = scanner.nextLine().replace(" ", "").toUpperCase();
+            }
+        }
+
+        System.out.println("[Valid key should be between 1 and 25.]");
         System.out.print("Enter the key: ");
         int key = scanner.nextInt();
 
         while (!isValidKey(key)){
-            System.out.println("Valid key should be between 1 and 25");
+            System.out.println("[Invalid choice. Valid key should be between 1 and 25.]");
             System.out.print("Enter the key: ");
             key = scanner.nextInt();
         }
 
-        if(choice.equals("E")){
-            CaesarC.encrypt(message,key);
-        } else if (choice.equals("D")) {
-            CaesarC.decrypt(message, key);
+        switch (choice) {
+            case "e" -> CaesarC.encrypt(message, key);
+            case "d" -> CaesarC.decrypt(message, key);
+            case "E" -> CaesarC.encrypt(message, key, key2);
+            case "D" -> CaesarC.decrypt(message, key, key2);
         }
     }
 }
